c7aa8a72360ec396ca329da4da098b36da97bbf7
Adarsh Inaganti <adarshinaganti006@gmail.com>
2021-06-17 16:48:37 +0530

    removed some folders

--- CodingFun/Tables.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter a number: "))
-i = int(1)
-while i <= 10:
-	print(num + " X " + i + " = " + num * i)
-	i = i + 1
--- Java/src/ForLoop.java
+++
@@ -1,8 +0,0 @@
-// Infinite for loop - Java
-public class ForLoop{
-    public static void main(String[] args){
-        for(int i = 0; i >= 0; i++){
-            System.out.println(i);
-        }
-    }
-}
