80bd4d2434551b4cf9891635c3fd5541446fde7e
Adarsh Inaganti <adarshinaganti006@gmail.com>
2021-06-27 10:29:51 +0530

    updated readability

--- CS50x/pset2/readability/readability.c
+++ CS50x/pset2/readability/readability.c
@@ -1,73 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 #include <math.h>
 
-int getLetters(string text);
-int getSentences(string text);
-int getWords(string text);
-int getGrade(int letters, int sentences, int words);
-void printGrade(int grade);
-
-int main(int argc, string argv[]){
-	string text = get_string("Text: ");
-	
-	int letters = getLetters(text);
-	int sentences = getSentences(text);
-	int words = getWords(text);
-	int grade = getGrade(letters, sentences, words);
-	
-	printGrade(grade);
-}
-
-void printGrade(int grade){
-	if(grade <= 1){
-		printf("Before Grade 1\n");
+int main(void){
+	string s = get_string("Text: ");
+	int numWords, numSentences, numLetters;
+	numWords = numLetters = numSentences = 0;
+	for(int i = 0, len = strlen(s); i < len; i++){
+		if(isalpha(s[i])) numLetters++;
+		if((i == 0 && s[i] != ' ') || (i != len - 1 && s[i] == ' ' && s[i + 1] != ' ')) numWords++;
+		if(s[i] == '.' || s[i] == '?' || s[i] == '!') numSentences++;
 	}
-
-	else if(grade >= 16){
-		printf("Grade 16+\n");
-	}
-
-	else{
-		printf("Grade %i\n", grade);
-	}
-}
-
-int getGrade(int letters, int sentences, int words){
-	float S = (sentences / (float) words) * 100;
-	float L = (letters / (float) words) * 100;
-	int grade = 0.0588 * L - 0.296 * S - 15.8;
-	return round(grade + 1);
-}
-
-int getLetters(string text){
-	int letters = 0;
-	for(int i = 0, n = strlen(text); i < n; i++){
-		if(isalpha(text[i])){
-			letters++;
-		}
-	}
-	return letters;
-}
-
-int getWords(string text){
-	int words = 0;
-	for(int i = 0, n = strlen(text); i < n; i++){
-		if(text[i] == ' '){
-			words++;
-		}
-	}
-	return words + 1;
-}
-
-int getSentences(string text){
-	int sentences = 0;
-	for(int i = 0, n = strlen(text); i < n; i++){
-		if(text[i] == '.' || text[i] == '!' || text[i] == '?'){
-			sentences++;
-		}
-	}
-	return sentences;
+	float L = (numLetters / (float) numWords) * 100;
+	float S = (numSentences / (float) numWords) * 100;
+	int index = round(0.0588 * L - 0.296 * S - 15.8);
+	if(index < 1) printf("Before Grade 1\n");
+	else if(index >= 16) printf("Grade 16+\n");
+	else printf("Grade %i\n", index);
 }
